a8eaee3c41e18327bdf6207f4ce5b75fe7530826
Petr Ptacek <petr.ptacek@olc.cz>
2023-06-23 17:34:52 +0200

    rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,32 @@
 import { defineConfig } from "rollup";
-import nodeResolve      from "@rollup/plugin-node-resolve";
-import commonjs         from "@rollup/plugin-commonjs";
-import pkg              from "./package.json" assert { type: "json" };
 
-export default defineConfig([
-    {
-      input: "src/index.js",
-      output: {
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs    from "@rollup/plugin-commonjs";
+import json        from "@rollup/plugin-json";
+import terser      from "@rollup/plugin-terser";
+
+import pkg from "./package.json" assert { type: "json" };
+
+export default defineConfig({
+    input: "src/index.js",
+    output: [
+      {
         name: "math",
-        file: pkg.browser,
-        format: "umd"
+        file: "./dist/math.umd.js",
+        format: "umd",
+        plugins: [
+          terser()
+        ]
       },
-      plugins: [
-        nodeResolve(),
-        commonjs()
-      ]
-    },
-    {
-      input: "src/index.js",
-      external: [
-        "lodash-es"
-      ],
-      output: [
-        {
-          file: pkg.main,
-          format: "cjs"
-        },
-        {
-          file: pkg.module,
-          format: "es"
-        }
-      ]
-    }
-  ]
+      {
+        file: "./dist/math.cjs.js",
+        format: "cjs"
+      }
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      json()
+    ]
+  }
 );
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { add }      from "./add.js";
 export { sum }      from "./sum.js";
 export { multiply } from "./multiply.js";
+export { VERSION }  from "./version.js";
